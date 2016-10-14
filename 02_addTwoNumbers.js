36619b7498529e753f9cae08385fe7c4c0d63763
wangning0 <172234437@qq.com>
2016-10-14 11:07:33 +0800

    优化

--- 02_addTwoNumbers.js
+++ 02_addTwoNumbers.js
@@ -10,6 +10,8 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+ //第一种解法
 var addTwoNumbers = function(l1, l2) {
     var arr1 = [];
     var arr2 = [];
@@ -65,3 +67,65 @@ var addTwoNumbers = function(l1, l2) {
     }
     return head;
 };
+
+//第二种解法 
+
+var addTwoNumbers = function(l1,l2) {
+    var add = 0, ans, head;
+    while(l1 || l2) {
+        var a = l1 ? l1.val : 0;
+        var b = l2 ? l2.val : 0;
+        
+        var sum = a + b + add;
+        add = ~~(sum / 10);
+
+        var node = new ListNode(sum%10);
+
+        if(!ans) {
+            ans = head = node;
+        } else {
+            head.next = node ;
+            head = node;
+        }
+
+        if(l1)
+            l1 = l1.next;
+        if(l2)
+            l2 = l2.next;
+    }
+
+    if(add) {
+        var node = new ListNode(add);
+        head.next = node;
+        head = node;
+    }
+
+    return ans;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
